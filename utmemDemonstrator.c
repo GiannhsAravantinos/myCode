4ada7cbecbb840adc9bdf42d3aef55555ea37c54
GiannhsAravantinos <johnnykef@hotmail.com>
2020-08-28 11:49:36 +0300

    A

--- utmemDemonstrator.c
+++ utmemDemonstrator.c
@@ -1,1 +1,21 @@
-init
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+
+#include "tmem.h"
+
+#define SPACES ("***********")
+
+int main (){
+
+  printf("%sHi, I will demonstrate how utmem works.%s",SPACES,SPACES)
+  return 0;
+}
